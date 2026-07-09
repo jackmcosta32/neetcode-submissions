650aa62060b6e12eb60e65781915225e8378857d
João Costa <42323045+jackmcosta32@users.noreply.github.com>
2026-07-08 23:24:30 -0300

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.ts
@@ -0,0 +1,34 @@
+class TreeNode<TValue> {
+    public left: TreeNode<TValue> | null
+    public right: TreeNode<TValue> | null
+    
+    constructor(public val: TValue | null) {}
+}
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums: number[]): number[][] {
+        const res: number[][] = []
+        const subset: number[] = []
+
+        const dfs = (i: number) => {
+            if (i >= nums.length) {
+                res.push([...subset])
+                return
+            }
+
+            subset.push(nums[i])
+            dfs(i + 1)
+
+            subset.pop()
+            dfs(i +1)
+        }
+
+        dfs(0)
+
+        return res
+    }
+}
